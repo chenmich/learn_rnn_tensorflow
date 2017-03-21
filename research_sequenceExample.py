49d8c9d04d1020b88f0958b88ce39b1145ce5e22
chenmich <403189920@qq.com>
2017-03-21 18:08:14 +0800

    Temporary storage

--- research_sequenceExample.py
+++ research_sequenceExample.py
@@ -22,6 +22,7 @@ t0.feature.add().float_list.value.append(2.0)
 l0.feature.add().float_list.value.append(3.0)
 t1.feature.add().float_list.value.append(4.0)
 l1.feature.add().float_list.value.append(5.0)
+print(ex)
 
 context_features = {
     "length_fea": tf.FixedLenFeature([], dtype=tf.int64),
@@ -35,6 +36,9 @@ sequence_features = {
 }
 
 context_parsed, sequence_parsed = tf.parse_single_sequence_example(
+    #note:SequenceExample() must be serialized before write to file
+    #and after read from file is serialized
+    #this function parse the serialized SequenceExample
     serialized=ex.SerializeToString(),
     context_features=context_features,
     sequence_features=sequence_features
@@ -42,7 +46,7 @@ context_parsed, sequence_parsed = tf.parse_single_sequence_example(
 
 print()
 print('================================================================')
-
+print(ex)
 print(context_parsed)
 print()
 print()
@@ -50,13 +54,26 @@ print(sequence_parsed)
 print()
 print()
 
+
 with tf.Session() as sess:
     print("length_fea    ", sess.run(context_parsed['length_fea']))
     print("length_seq    ", sess.run(context_parsed["length_seq"]))
     print()
-    print("tokens0     ", sess.run(sequence_parsed["tokens0"]))
+    #after this code run, a python list will be gotten 
+    sequence_token0 = sess.run(sequence_parsed["tokens0"])
+    print("tokens0     ", sequence_token0)
     print("tokens1     ", sess.run(sequence_parsed["tokens1"]))
-    pass
+    print()
+
+
+#below code is not for practice
+another_ex = tf.train.SequenceExample()
+seq_feature = ex.feature_lists.feature_list["tokens0"]
+#some_feature object has not attribte features
+#some_feature = ex.features.feature_lists.feature_list['some']
+#some_feature.feature.add().float_list.value.append(5.0)
+
+
 
 
 
